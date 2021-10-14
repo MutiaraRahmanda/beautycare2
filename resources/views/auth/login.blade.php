da9670d74ca7aa375d00815e6c94d3a64d406335
MutiaraRahmanda <mutiararizkia18@gmail.com>
2021-10-15 02:08:17 +0700

    Modifikasi Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
                 <div class="row justify-content-md-center">
                     <div class="card-wrapper col-12 col-md-4 mt-5">
                         <div class="brand text-center mb-3">
-                            <a href="/"><img src="{{ asset('img/logo.png') }}"></a>
+                            <a href="/"><img src ="assets/images/relax.png" width="100px"><h1>Beauty Care</h1></a>
                         </div>
                         <div class="card">
                             <div class="card-body">
@@ -35,7 +35,7 @@
                                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                                             name="email" value="{{ old('email') }}" required
                                             autocomplete="email" autofocus>
-        
+
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -47,7 +47,7 @@
                                         <label for="password">Password</label>
                                         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"
                                             required autocomplete="current-password">
-        
+
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
