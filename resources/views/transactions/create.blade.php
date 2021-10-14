906fa1e1612d0e0ceba350a0ad9838ba7428ea1f
MutiaraRahmanda <mutiararizkia18@gmail.com>
2021-10-15 03:22:11 +0700

    Modifikasi form reservasi

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -2,35 +2,22 @@
 
 @section('content')
 <!-- Breadcrumb End -->
-<div class="breadcrumb-option set-bg" data-setbg="{{ asset('img/breadcrumb-bg.jpg') }}">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <div class="breadcrumb__text">
-                    <h2>Form Pembelian</h2>
+                    <h2>Form Reservasi</h2>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 <!-- Breadcrumb Begin -->
 
 <!-- Contact Section Begin -->
 <section class="contact spad">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 col-md-6">
-                <div class="contact__text">
-                    <div class="section-title">
-                        <h2>{{ $brand->nama }}</h2>
-                        <p>Rp {{ number_format($brand->biaya, 0, ',', '.') }}</p>
-                    </div>
-                    <ul>    
-                        <li><span>Nama Dokter</span> {{ $car->nama }}</li>
-                        <li><span>Spesialist</span> {{ $car->specialist }}</li>
-                    </ul>
-                </div>
-            </div>
             <div class="col-lg-6 col-md-6">
                 <div class="contact__form">
                     <form action="{{ route('transactions.store') }}" method="POST">
@@ -45,21 +32,6 @@
                                 <input type="text" name="nama" id="nama">
                             </div>
                         </div>
-
-                        <textarea placeholder="Alamat" name="alamat"></textarea>
-
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <label for="no_hp">No. Telp</label>
-                                <input type="text" name="no_hp" id="no_hp">
-                            </div>
-                            <div class="col-lg-6">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" id="email">
-                            </div>
-                        </div>
-
-                        
                         <div class="row">
                             <div class="col-lg-6">
                                 <label for="jenis_kelamin">Jenis Kelamin</label>
@@ -73,28 +45,18 @@
                                 <input type="date" name="tanggal_lahir" id="tanggal_lahir">
                             </div>
                         </div>
-
-
+                        <textarea placeholder="Alamat" name="alamat"></textarea>
                         <div class="row">
                             <div class="col-lg-6">
-                                <label for="jenis_reservasi">Jenis Reservasi</label>
-                                <select name="jenis_reservasi" id="jenis_reservasi">
-                                    <option value="Treatment">Treatment</option>
-                                    <option value="Konsultasi">Konsultasi</option>
-                                </select>
+                                <label for="no_hp">No. Telp</label>
+                                <input type="text" name="no_hp" id="no_hp">
                             </div>
                             <div class="col-lg-6">
-                                <label for="tanggal">Tanggal</label>
-                                <input type="date" name="tanggal" id="tanggal">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" id="email">
                             </div>
                         </div>
-
-
                         <div class="row">
-                            <div class="col-lg-6">
-                                <label for="waktu">Waktu</label>
-                                <input type="time" name="waktu" id="waktu">
-                            </div>
                             <div class="col-lg-6">
                                 <label for="jenis_pembayaran">Jenis Pembayaran</label>
                                 <select name="jenis_pembayaran" id="jenis_pembayaran">
@@ -102,9 +64,28 @@
                                     <option value="Transfer">Transfer</option>
                                 </select>
                             </div>
+                            <div class="col-lg-6">
+                                <label for="jenis_reservasi">Jenis Reservasi</label>
+                                <select name="jenis_reservasi" id="jenis_reservasi">
+                                    <option value="Treatments">Treatments</option>
+                                    <option value="Konsultasi">Konsultasi</option>
+                                </select>
+                            </div>
                         </div>
-                        <input type="hidden" name="treatment_id" value="{{ $car->id }}">
-                        <button type="submit" class="site-btn">Pesan Sekarang</button>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label for="waktu">Waktu</label>
+                                <input type="time" name="waktu" id="waktu">
+                            </div>
+                            <div class="col-lg-6">
+                                <label for="tanggal_reservasi">Tanggal Reservasi</label>
+                                <input type="date" name="tanggal_reservasi" id="tanggal_reservasi">
+                            </div>
+                        </div>
+                        {{-- <input type="hidden" name="car_id" value="{{ $car->id }}">
+                        <input type="hidden" name="brand_id" value="{{ $brand->id }}"> --}}
+                        {{-- <a href="{{ route('transactions.nota') }}" class="primary-btn"> --}}
+                            <button type="submit" class="site-btn">Pesan Sekarang</button>
                     </form>
                 </div>
             </div>
